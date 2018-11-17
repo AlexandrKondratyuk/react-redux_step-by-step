491cdc73ad29b3471d5d90e311cdbb30f6b09ddb
System Administrator <root@MacBook-Air-Alexandr.local>
2018-11-17 21:31:23 +0200

    react-redux => dispatch params

--- src/App.js
+++ src/App.js
@@ -12,9 +12,13 @@ class App extends Component {
         <hr/>
 
         <div className="Actions">
-          <button onClick={this.props.onAdd}>Добавить 1</button>
-          <button onClick={this.props.onSub}>Вычесть 1</button>
+          <button onClick={this.props.onAdd}>Add 1</button>
+          <button onClick={this.props.onSub}>Subtract 1</button>
         </div>
+          <div className="Actions">
+              <button onClick={this.props.onAddNumber.bind(this, 5)}>Add 5</button>
+              <button onClick={() => this.props.onSubNumber(7)}>Subtract 7</button>
+          </div>
       </div>
     )
   }
@@ -29,7 +33,9 @@ function mapStateToProps(state) {
 function mapDispatchToProps(dispatch) {
     return {
         onAdd: () => dispatch({type: 'ADD'}),
-        onSub: () => dispatch({type: 'SUB'})
+        onSub: () => dispatch({type: 'SUB'}),
+        onAddNumber: number => dispatch({type: 'ADD_NUMBER', payload: number}),
+        onSubNumber: number => dispatch({type: 'SUB_NUMBER', payload: number}),
     }
 }
 
